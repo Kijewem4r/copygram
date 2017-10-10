ad226c761ba9fd2d3ba2736b86940a228a8e480b
Andrey Motov <tovrey@gmail.com>
2017-10-10 17:42:05 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+API_ID = dddddd
+API_HASH = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ROOT_DIR = "/path/to/dir/"
+SESSION_NAME = "username"
+ITERATIONS = 2
+MESSAGE_LIMIT = 200
+PAUSE = 10
+PHONE = "+ddddddddddd"
